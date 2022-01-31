ec8e5529c8fc736338b26d5f5075a52163905930
kylo252 <59826753+kylo252@users.noreply.github.com>
2022-01-31 11:48:17 +0100

    fix(ci): better shell options for windows

--- scripts/minimal_init.lua
+++ scripts/minimal_init.lua
@@ -7,6 +7,17 @@ vim.cmd [[au BufRead,BufNewFile *.conf set filetype=hocon]]
 vim.o.swapfile = false
 vim.bo.swapfile = false
 
+-- luacheck: ignore 631
+vim.cmd [[
+    if has('win32')
+		  let &shell = has('win32') ? 'powershell' : 'pwsh'
+      let &shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;'
+      let &shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+      let &shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+      set shellquote= shellxquote=
+    endif
+]]
+
 require("nvim-treesitter.configs").setup {
   indent = { enable = true },
   highlight = { enable = true },
